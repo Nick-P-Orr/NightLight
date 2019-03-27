1f95747d29bd4e83a5ee04facd3e44f06f300f41
unknown <nick.p.orr@gmail.com>
2019-03-27 00:50:38 -0400

    Initial Commit

---
+++ NightLight.m
@@ -0,0 +1,13 @@
+%Computer Vision Final Project - 'Night Light'
+%Nick Orr, orrn@wit.edu,
+%Brendan Sileo, sileob@wit.edu
+
+clc; close all; clear all;
+
+A = imread('Night_Sample1.png');
+B = imread('Night_Sample2.jpg');
+disp('test');
+
+
+pause;
+clc; close all; clear all;
