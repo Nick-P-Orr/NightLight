a40a1c717a0b8ce72a313a14a3395aff42ba5e39
Nick <nick.p.orr@gmail.com>
2019-04-10 14:15:05 -0400

    Started adding webcam functionality

--- NightLight.m
+++ NightLight.m
@@ -10,8 +10,31 @@ function simpleGUI
     movegui(hFig,'center')          %# Move the GUI to the center of the screen
 
     hBtnGrp = uibuttongroup('Position',[0 0 0.3 1], 'Units','Normalized');
-    uicontrol('Style','Radio', 'Parent',hBtnGrp, 'HandleVisibility','off', 'Position',[15 150 70 30], 'String','Load Image', 'Tag','li')
-    uicontrol('Style','Radio', 'Parent',hBtnGrp, 'HandleVisibility','off', 'Position',[15 120 70 30], 'String','Quit', 'Tag','q')
+    uicontrol('Style','Radio', 'Parent',hBtnGrp, 'HandleVisibility','off', 'Position',[15 150 70 30], 'String','Load Image', 'Tag','s')
+    uicontrol('Style','Radio', 'Parent',hBtnGrp, 'HandleVisibility','off', 'Position',[15 120 70 30], 'String','Web Cam', 'Tag','w')
+    uicontrol('Style','Radio', 'Parent',hBtnGrp, 'HandleVisibility','off', 'Position',[15 90 70 30], 'String','Quit', 'Tag','q')
+    
+
+    uicontrol('Style','pushbutton', 'String','Choose', 'Position',[15  60 70 30], 'Callback',{@button_callback})
+
+    set(hFig, 'Visible','on')        %# Make the GUI visible
+
+    %# callback function
+    function button_callback(src,ev)
+        switch get(get(hBtnGrp,'SelectedObject'),'Tag')
+            case 'w', webcamImage();
+            case 's', selectImage();
+            case 'q',  return;
+        end
+    end
+end
+
+function camGUI
+    hFig = figure('Visible','off', 'Menu','none', 'Name','Night Light', 'Resize','off', 'Position',[100 100 1050 600]);
+    movegui(hFig,'center')          %# Move the GUI to the center of the screen
+
+    hBtnGrp = uibuttongroup('Position',[0 0 0.3 1], 'Units','Normalized');
+    uicontrol('Style','Radio', 'Parent',hBtnGrp, 'HandleVisibility','off', 'Position',[15 150 70 30], 'String','Take Picture', 'Tag','tp')
 
     uicontrol('Style','pushbutton', 'String','Choose', 'Position',[15  90 70 30], 'Callback',{@button_callback})
 
@@ -20,15 +43,26 @@ function simpleGUI
     %# callback function
     function button_callback(src,ev)
         switch get(get(hBtnGrp,'SelectedObject'),'Tag')
-            case 'li', processImage();
-            case 'q',  return;
+            case 'tp', img = snapshot(cam);
         end
     end
 end
 
-function processImage
+function selectImage
     name = uigetfile('*');
-    A = imread(name);
+    selectedImage = imread(name);
+    processImage(selectedImage);
+end
+
+function webcamImage
+    cam = webcam;
+    camGUI;
+    preview(cam);
+    
+
+end
+
+function processImage(A) 
     ABlack = rgb2gray(A);
     
     [wi, hi] = size(ABlack);
